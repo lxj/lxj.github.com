85831360312f75774c8c56ab7f8f5d4cc4a6f77a
lxj <louxiaojian@gmail.com>
2015-02-28 20:23:44 +0800

    update

--- assets/app/notes/cssURL.js
+++ assets/app/notes/cssURL.js
@@ -1,1 +1,2 @@
-document.writeln("<link rel=\"stylesheet\" rev=\"stylesheet\" href=\"http:\/\/www.lxj.com\/front-end-development\/webStandards\/case_style\/case.css\" type=\"text\/css\" media=\"all\" \/>");
+document.writeln('<link rel="stylesheet" rev="stylesheet" href="http://www.lxj.com/front-end-development/webStandards/case_style/case.css" type="text/css" media="all" />');
+/**Create by Fep at 20150228 20:20:03**/
